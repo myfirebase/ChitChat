ced19900f39bfd3c0d711ddb80a67d4f330ef203
Amrani Houssain <amranidev@gmail.com>
2017-04-09 23:13:41 +0100

    init components

---
+++ components/Landing.vue
@@ -0,0 +1,51 @@
+<template>
+<div>    
+<navbar></navbar>
+<transition name="fade" tag="div">
+    <router-view></router-view>
+</transition>
+</div>
+</template>
+<script>
+
+import navbar from './../components/partials/Navbar'
+
+export default {
+    mounted() {
+        this.$auth.check({
+            then: (user) =>{
+                console.log("logged in")
+            },
+            catch: ()=> {}
+        });
+    },
+    data() {
+        return {
+            message: "Welcome Myfirebase SPA framework",
+            userLogged: false
+        }
+    },
+    methods: {},
+    components: {
+        navbar
+    }
+}
+</script>
+
+
+<style>
+.flex{
+  height: 500px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+/*
+.fade-enter, .fade-leave-active {
+  transition: opacity 5s;
+}
+.fade-enter-active, .fade-leave-to {
+  transform: translateY(100px);
+  opacity: 0;
+}*/
+</style>
---
+++ components/partials/Navbar.vue
@@ -0,0 +1,82 @@
+<template>
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" @click='collapsAction'>
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">{{Logo}}</a>
+		</div>
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div :class="collaps">
+			<ul class="nav navbar-nav">
+				<li><router-link to="/">Home</router-link></li>
+				<li><router-link to="/app">App</router-link></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li class="">
+					<a href="#" role="button" @click = "toggle = !toggle">{{username}} <span class="caret"></span></a>
+					<ul v-if="toggle" class="dropdown-menu">
+						<li v-if = "!signed"><router-link to = "login">Login</router-link></li>
+						<li v-if = "signed"><router-link to = 'update-profile'>Profile</router-link></li>
+						<li role="separator" class="divider"></li>
+						<li v-if = "signed"><a href="#" @click="logout()">Logout</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+</template>
+
+<script>
+export default {
+    mounted() {
+    	this.$auth.state({
+            forward: '/update-profile',
+            redirect: '/login',
+    		then: (user) => {
+    			this.signed = true
+    			this.username = user.displayName
+    		},
+    		catch: () => {
+    			this.username = "Auth"
+    		}
+    	})
+    },
+    firebase() {
+        return {
+            activeUsers: this.$store.state.database.child('activeUsers')
+        }
+    },    
+    data() {
+        return {
+            Logo: "Myfirebase",
+            toggle: false,
+            signed: false,
+            username: '',
+            collapsClass: 'collapse navbar-collapse'
+        }
+    },
+    computed:{
+    	collaps()
+    	{
+    		return this.collapsClass
+    	}
+    },
+    methods:{
+    	logout(){
+            this.$firebaseRefs.activeUsers.child(this.$auth.user().uid).remove()
+            this.$auth.logout()
+    		this.signed = false
+    	},
+    	collapsAction(){
+    		this.collapsClass = this.collapsClass ? '' : 'collapse navbar-collapse'
+    	}
+    }
+}
+</script>
