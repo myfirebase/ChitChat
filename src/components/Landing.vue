9c0fac08fa8b3002485396f8765cfa0dc19a3b00
Amrani Houssain <amranidev@gmail.com>
2017-05-14 23:10:31 +0100

    update landing component

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,36 +1,37 @@
 <template>
-<div>
-<navbar></navbar>
-    <router-view></router-view>
-</div>
+    <div>
+        <navbar></navbar>
+        <router-view></router-view>
+    </div>
 </template>
+
 <script>
-import navbar from './../components/partials/Navbar'
-export default {
-    mounted() {
-        this.$auth.check({
-            then: (user) =>{
-                // User loggedin
-            },
-            catch: ()=> {}
-        });
-    },
-    data() {
-        return {
-            message: "Welcome Myfirebase SPA framework",
-            userLogged: false
+    import navbar from './../components/partials/Navbar'
+    export default {
+        mounted() {
+            this.$auth.check({
+                then: (user) => {
+                    // User loggedin
+                },
+                catch: () => {}
+            });
+        },
+        data() {
+            return {
+                message: "Welcome Myfirebase SPA framework",
+                userLogged: false
+            }
+        },
+        methods: {},
+        components: {
+            navbar
         }
-    },
-    methods: {},
-    components: {
-        navbar
     }
-}
 </script>
 
 
 <style>
-    .flex{
+    .flex {
         height: 500px;
         display: flex;
         align-items: center;
