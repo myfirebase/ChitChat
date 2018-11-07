54bf35d19bf04d4b00143bfea950879940da5cc0
Amrani Houssain <amranidev@gmail.com>
2018-11-07 22:24:07 +0000

    setup routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,10 +3,9 @@ import Layout from '@/components/Layout'
 import Welcome from '@/components/Welcome'
 import Login from '@/components/auth/Login'
 import Profile from '@/components/auth/UpdateProfile'
-import UpdateProfile from '@/components/auth/UpdateProfile'
 import App from '@/components/App'
 import NotFound from '@/components/errors/404'
-import Firestore from '@/components/Firestore'
+import Room from '@/components/Room'
 
 import Vue from 'vue'
 
@@ -24,24 +23,8 @@ const routes = [{
             name: 'login'
         },
         {
-            path: '/update-profile',
-            component: UpdateProfile,
-            name: 'Profile Update',
-            meta: {
-                auth: true
-            }
-        },
-        {
-            path: '/realtime-database',
-            component: App,
-            name: 'Realtime Database',
-            meta: {
-                auth: true
-            }
-        },
-        {
-            path: '/firestore',
-            component: Firestore,
+            path: '/room',
+            component: Room,
             name: 'Cloud Firestore',
             meta: {
                 auth: true
