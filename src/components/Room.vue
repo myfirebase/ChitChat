930ce1de61507378b3126d7943adc8c30d526337
Amrani Houssain <amranidev@gmail.com>
2018-11-14 21:49:27 +0000

    Update src/components/Room.vue
    WIP, working on updating messages

--- src/components/Room.vue
+++ src/components/Room.vue
@@ -7,12 +7,13 @@
         <v-list-tile-avatar  color="grey lighten-4" v-if="thread.uid !== user.uid">
           <img :src="thread.photoURL" alt="avatar">
         </v-list-tile-avatar>
-          <v-list-tile-content>
-            <v-list-tile-title>{{ thread.message }}</v-list-tile-title>
+          <v-list-tile-content @dblclick='editThread(thread)'>
+            <v-list-tile-title v-if='!thread.show'>{{ thread.message }}</v-list-tile-title>
+            <v-text-field v-else='thread.show' v-model='Thread.message'></v-text-field>
           </v-list-tile-content>
-          <!-- <v-list-tile-action v-if="thread.uid === user.uid">
-            <v-icon color="black" @click="remove(thread['.key'])">delete</v-icon>
-          </v-list-tile-action> -->
+          <v-list-tile-action v-if="thread.show">
+            <v-icon color="black" @click='updateThread(thread)'>save</v-icon>
+          </v-list-tile-action>
         <v-list-tile-avatar  color="grey lighten-4" v-if="thread.uid === user.uid">
           <img :src="thread.photoURL" alt="avatar">
         </v-list-tile-avatar>
@@ -39,28 +40,44 @@ import Room from '@/mixin/room'
 export default {
     mixins: [Room],
     data () {
-        return {
-            Thread: new Thread(this.$myFirestore.collection('Threads')).init(),
-            ready: false
-        }
+      return {
+          Thread: new Thread(this.$myFirestore.collection('Threads')).init(),
+          ready: false
+      }
     },
     methods: {
         newMessage () {
-            if (this.Thread.isEmpty()) {
-              return
-            }
-            this.Thread.setCreatedAt(this.timestamp);
-            this.Thread.add().then((success) => {
-            }).catch(error => {
-                console.log(error.message)
-            })
-            this.Thread.clearMessage()
+          if (this.Thread.isEmpty()) {
+            return
+          }
+          this.Thread.setCreatedAt(this.timestamp);
+          this.Thread.add().then((success) => {
+          }).catch(error => {
+              console.log(error.message)
+          })
+          this.Thread.clearMessage()
         },
         remove (key) {
-            this.Thread.delete(key).then(() => {
-            }).catch(error => {
-                console.log(error.message)
-            })
+          this.Thread.delete(key).then(() => {
+          }).catch(error => {
+              console.log(error.message)
+          })
+        },
+        editThread (message) {
+          message.show = true
+          this.Thread.setMessage(message.message)
+          this.$forceUpdate()
+        },
+        updateThread (message) {
+          console.log(message['.key'])
+          // console.log(this.Thread)
+          this.Thread.update(message['.key']).then(() => {
+            // console.log('Done')
+          }).catch((err) => {
+            // console.log('errr')
+          })
+          message.show = false
+          this.$forceUpdate()
         }
     }
 }
